3c05299dd7836d54e237b3f028e7aa3085d33c5f
lcm098 <lastnightsprit@gmail.com>
2026-08-22 19:14:54 +0530

    Working

--- include/utility.hpp
+++ include/utility.hpp
@@ -6,11 +6,19 @@
 #include "utils.hpp"
 #include "../__cuda__/include/kernel.cuh"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 TensorEngine* T(TensorEngine * t, int *axes = nullptr);
 TensorEngine* transpose(TensorEngine * t, int *axes = nullptr);
 TensorEngine* arange(f64 start, f64 end, f64 step, bool gpu);
 TensorEngine* reshape(TensorEngine* t, int *new_shape, size_t new_ndim);
 TensorEngine* linspace(f64 start, f64 end, int num, bool __GPU__) ;
 TensorEngine* slice(TensorEngine* t, int indices[][3], size_t num_slices, int offset);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
